009aa43911c9edfd8e6256906fd71f13f8c8b3c7
Marcin Bojarski <m.bojarski@jds-group.eu>
2024-04-06 01:49:55 +0200

    Code cleanup

--- class/jd_assets.php
+++ class/jd_assets.php
@@ -15,10 +15,25 @@ class jd_assets
         wp_enqueue_style('jd-style', plugin_dir_url(__DIR__).'assets/scss/admin.css', [], date('YmdHi '));
         wp_enqueue_style('fa', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css');
         wp_enqueue_script('jd-admin-js', plugin_dir_url(__DIR__).'assets/js/admin.js', [], date('YmdHi'));
-        wp_enqueue_script('sweetalert2', 'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.7/dist/sweetalert2.all.min.js', [], '11.10.7', true);
-        wp_enqueue_style('animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css', [], '4.1.1');
-
-        wp_enqueue_style('sweetalert2-css', 'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.7/dist/sweetalert2.min.css', [], '11.10.7');
+        wp_enqueue_script(
+            'sweetalert2',
+            'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.7/dist/sweetalert2.all.min.js',
+            [],
+            '11.10.7',
+            true
+        );
+        wp_enqueue_style(
+            'animate-css',
+            'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
+            [],
+            '4.1.1'
+        );
+        wp_enqueue_style(
+            'sweetalert2-css',
+            'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.7/dist/sweetalert2.min.css',
+            [],
+            '11.10.7'
+        );
     }
 
     public function custom_enqueue_checkout_script(): void
@@ -27,7 +42,6 @@ class jd_assets
             wp_enqueue_script('jd-checkout-js', plugin_dir_url(__DIR__).'assets/js/jdcustom.js', null, '45');
         }
         wp_enqueue_script('js-general-js', plugin_dir_url(__DIR__).'assets/js/global.js', null, '2.1');
-
         wp_enqueue_style('jd-style', plugin_dir_url(__DIR__).'assets/css/jd-checkout.css', false, '1.3', 'all');
     }
 }
