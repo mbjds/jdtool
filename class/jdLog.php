9ce3f544466fd503e18124abb4f87d6c8de36515
Marcin Bojarski <m.bojarski@jds-group.eu>
2024-04-09 11:21:32 +0200

    Create log class based on Monolog.

---
+++ class/jdLog.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace JDCustom;
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
+
+class jdLog
+{
+    private $logger;
+
+    public function __construct()
+    {
+        $dateFormat = 'Y-m-d h:m:s';
+        $stream = new StreamHandler(WP_CONTENT_DIR.'/vouchers.log', Level::Debug);
+        $output = "%datetime% | %level_name% |  %message% \n";
+        $stream->setFormatter(new LineFormatter($output, $dateFormat, true, true));
+        $this->logger = new Logger('jd-log');
+        $this->logger->pushHandler($stream);
+    }
+
+    public function logInfo($message)
+    {
+        $this->logger->info($message);
+    }
+
+    public function logWarning($message)
+    {
+        $this->logger->warning($message);
+    }
+
+    public function logError($message)
+    {
+        $this->logger->error($message);
+    }
+
+    public function logCritical($message)
+    {
+        $this->logger->critical($message);
+    }
+}
